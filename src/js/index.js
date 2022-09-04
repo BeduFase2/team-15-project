76494441c2e5f8c5c61a1d79729a0d5ba8bbb1fd
Omar Guzmán <33527049+jomarguzman@users.noreply.github.com>
2022-09-04 09:15:39 -0500

    add click listener and retrieve seach box text

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,11 @@
 import 'bootstrap';
 import '../scss/styles.scss';
+
+const searchButton = document.getElementById('searchButton');
+const searchText = document.getElementById('searchField');
+
+searchButton.addEventListener("click", (event) => {
+    
+    searchText.value = '';
+    event.preventDefault();
+}, false);
